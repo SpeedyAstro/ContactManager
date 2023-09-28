2db54e125ed9afd5f1aaece5fa1ecd87e1955f60
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2023-09-28 08:12:17 +0530

    Image

--- src/main/java/in/astro/controller/UserController.java
+++ src/main/java/in/astro/controller/UserController.java
@@ -5,10 +5,18 @@ import in.astro.model.User;
 import in.astro.service.IContactService;
 import in.astro.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -38,13 +46,24 @@ public class UserController {
         return "normal/add_contact_form";
     }
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact, Principal principal){
-        String name = principal.getName();
-        System.out.println(contact);
-        User user = service.getUserByEmail(name);
-        contact.setUser(user);
-        user.getContacts().add(contact);
-        service.saveUser(user);
+    public String processContact(@ModelAttribute Contact contact,
+                                 @RequestParam("profileImage") MultipartFile file, Principal principal) throws IOException {
+        try {
+            String name = principal.getName();
+            User user = service.getUserByEmail(name);
+            if (file.isEmpty()) {
+            } else {
+                contact.setImage(file.getOriginalFilename());
+                File file1 = new ClassPathResource("static/img").getFile();
+                Path path = Paths.get(file1.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            }
+            user.getContacts().add(contact);
+            contact.setUser(user);
+            service.saveUser(user);
+        }catch (Exception e){
+            e.getMessage();
+        }
         return "normal/add_contact_form";
     }
 }
